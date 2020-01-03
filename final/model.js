45d5c9fcce6a4cb179130a7caff189838f31851f
asd153866714 <45963921+asd153866714@users.noreply.github.com>
2020-01-03 10:12:10 +0800

    Update model.js

--- final/model.js
+++ final/model.js
@@ -9,15 +9,7 @@ var client, db, logins
 M.open = async function () {
     client = await MongoClient.connect(url,{useUnifiedTopology: true, useNewUrlParser: true})
     db = await client.db(dbName) // 創建名為 'project' 資料庫
-    logins = await db.collection('logins') // 創建 db:logins 下的 colletion:login
-}
-
-M.clear = async function () {
-    await logins.drop()
-}
-
-M.close = async function () {
-    await client.close()
+    logins = await db.collection('logins') // 創建 db:project 下的 colletion:logins
 }
 
 M.add = async function (login) {
@@ -29,10 +21,4 @@ M.add = async function (login) {
 M.get = async function (Id,Password) {
     let data = await logins.findOne({id:Id, password:Password}) // data 取得資料庫對應的帳密
     return data
-}
-
-M.list = async function () {
-    var loginList = await logins.find({}).sort({ created_at: -1 }).toArray()
-    // console.log('postList = ', postList)
-    return loginList
 }
